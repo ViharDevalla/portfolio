8bee24aa2b73860155952db16eca63a3d5dd8ff9
Vihar Devalla <vihardevalla@gmail.com>
2020-09-17 22:19:24 +0530

    Favivon changed
    .

--- src/App.js
+++ src/App.js
@@ -2,9 +2,16 @@ import React from 'react';
 import './style.css';
 import Content from './Content';
 import Sidebar from './Sidebar.js';
+import {Helmet} from "react-helmet";
+
+
 function App() {
+
   return (
     <div className="main clearfix">
+      <Helmet>
+                <title>Vihar's Personal Portfolio</title>
+            </Helmet>
       <Sidebar />
       <Content />
     </div>
