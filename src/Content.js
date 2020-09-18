4603ac519725306e87fb7111a2b6444a9989cfdd
Vihar Devalla <vihardevalla@gmail.com>
2020-09-18 11:05:20 +0530

    Added ga

--- src/Content.js
+++ src/Content.js
@@ -19,6 +19,7 @@ function Content() {
                 <Projects />
                 <Achieve />
                 <Contact />
+                <div className="Credits">Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
             </div>
         </div>
     )
