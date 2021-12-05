1231fb77ebdcb4a8d09a95f06bdc9b80a322dff0
Vihar Devalla <52347065+ViharDevalla@users.noreply.github.com>
2021-12-05 19:45:53 +0530

    Update Profile.js

--- src/Profile.js
+++ src/Profile.js
@@ -6,7 +6,7 @@ function Profile() {
     return (
         <div className="box titlefont" id="profile">
             WHO AM I?
-            <p className="subfont">I am a sophomore from PES University studying Computer Sciene Engineering.</p><p className="subfont">I consider myself avid tech enthusiast with lifelong passion towards learning something new.</p><p className="subfont">I love the art of software development and find breaking past its security intriguing.</p>
+            <p className="subfont">I am a Junior from PES University studying Computer Sciene Engineering.</p><p className="subfont">I consider myself avid tech enthusiast with lifelong passion towards learning something new.</p><p className="subfont">I love the art of software development and find breaking past its security intriguing.</p>
             <p className="subfont">My CTF Handle is Viked Joker and I play for team nUb5.</p>
         </div>
     )
