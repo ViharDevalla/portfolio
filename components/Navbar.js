0b3e5ca047ca395df9cbe383f5162c7ef56128c7
Vihar Devalla <vihardevalla@gmail.com>
2023-03-19 18:07:26 +0530

    Logo Modified

--- components/Navbar.js
+++ components/Navbar.js
@@ -4,10 +4,9 @@ const Navbar = () => {
         <nav className="sticky top-0 z-20 bg-white backdrop-filter backdrop-blur-lg bg-opacity-5 border-b border-gray-800">
             <div className="max-w-full  md:px-48">
                 <div className="flex items-center justify-between h-16">
-                <div className="pl-5 md:pl-0 md:text-2xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r  from-cyan-300 to-sky-600">
-                    Vihar Devalla
+                <div className="flex items-center">
+                    <Image className="rounded-full" src="/logo_2.png" width={60} height={60} alt="logo" />
                 </div>
-
                 <div className="flex space-x-4 text-gray-300 uppercase font-semibold">
                     <a className='transition duration-300 ease-in-out hover:scale-105 hover:text-cyan-500' href="#">Blogs</a>
                     <a className='transition duration-300 ease-in-out hover:scale-105 hover:text-teal-500' href="#project">Projects</a>
